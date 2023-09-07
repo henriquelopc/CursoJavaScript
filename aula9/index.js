402789df3fbd9a8b7d5bd451d3249bde82eb156d
Henrique Lourenco <henriquelpcoelho@gmail.com>
2023-09-07 10:19:40 -0300

    Aula 9 tipos de dados primitivos

---
+++ aula9/index.js
@@ -0,0 +1,17 @@
+//tipos de dados primitivos
+// String, number, undefined, null, boolean, symbol
+const nome = 'Luiz'; //String
+const num = 10; // number
+const num2 = 10.52; // number
+let nomeAluno; // undefined  = nao aponta para local nenhum da memoria
+const sobrenomeAluno = null; // nulo = nao aponta para local nenhum da memoria
+const aprovado = true;//boolean (logico)
+
+console.log(typeof aprovado);
+
+const a =[1,2];
+const b = a;
+
+
+
+
