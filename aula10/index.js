69feee3cc34224ec537c43fba8f86d157815e4d3
Henrique Lourenco <henriquelpcoelho@gmail.com>
2023-09-07 10:27:20 -0300

    Aula 10 Operadores

---
+++ aula10/index.js
@@ -0,0 +1,24 @@
+/*
+Aritmeticos
++ adicao / concatenacao ( - / *)
+- subtracao/ *multiplicao
+** potenciacao
+% resto da divisao
+
+Incremento ++
+Decremento --
+
+NaN - Not a Number
+*/ 
+
+const num1 = 2;
+const num2 = 10;
+console.log(num1 ** num2);
+
+let contador = 1;
+contador++;
+console.log(contador);
+
+const num3 = parseInt('5');
+let conta = num3 + num2;
+console.log(conta);
